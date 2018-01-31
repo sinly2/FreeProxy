441a2281699a846357b6301f5d2906a568432610
sinly2 <15380794704@163.com>
2018-02-01 01:49:19 +0800

    取消http和https的区分，直接以dic_url的维度进行代理验证

--- base.py
+++ base.py
@@ -37,30 +37,32 @@ class Base(object):
                         "host": "www.xicidaili.com"}
 
     @staticmethod
-    def get_dynamic_source(seed_url, callback=None):
+    def get_dynamic_source(seed_url, callback=None, proxy=None):
         try:
             display = Display(visible=0, size=(800, 600))
             display.start()
-            driver = webdriver.Firefox()
+            if proxy:
+                profile = webdriver.FirefoxProfile()
+                profile.set_preference('network.proxy.type', 1)
+                profile.set_preference('network.proxy.http', proxy[0])
+                profile.set_preference('network.proxy.http_port', proxy[1])
+                profile.set_preference('network.proxy.ssl', proxy[0])
+                profile.set_preference('network.proxy.ssl_port', proxy[1])
+                profile.update_preferences()
+                driver = webdriver.Firefox(profile)
+            else:
+                driver = webdriver.Firefox()
             driver.get(seed_url)
             page_source = driver.page_source
-            # for cookie in self.driver.get_cookies():
-            #     self.cookies.update(dict(cookie["name"],cookie["value"]))
-            # cookies = requests.utils.cookiejar_from_dict(self.cookies, cookiejar=None, overwrite=True)
-            # self.session.cookies = cookies
-            # cookies = requests.utils.dict_from_cookiejar(self.session.cookies)
-            # with open("a.txt","wb") as fp:
-            #     fp.write(page_source)
             driver.quit()
             display.stop()
             if callback:
                 return callback(page_source)
             else:
                 return page_source
-
         except Exception, e:
-            print "[ERROR]Selenium visit %s failed..." % (seed_url)
             print e
+            print "[ERROR]Selenium visit %s failed..." % (seed_url)
             return "1010"
 
     def get_source(self, seed_url, callback=None):
@@ -85,34 +87,47 @@ class Base(object):
     def get_cookie(self):
         return self.session.cookies
 
+    def set_firefox_proxy(self):
+        pass
+
+
 
 def parse_func_1(page_source):
     # host http://www.xicidaili.com
+    #Return None or a list
     tree = lxml.html.fromstring(page_source)
     trs = tree.cssselect("table#ip_list")
     result = []
     for td in trs[0].getchildren()[0].getchildren():
-        result.append([td[1].text_content(),td[2].text_content()])
-    del result[0]
-    return result
+        #速度<1秒且存活时间>1小时
+        if u"速度" not in td[6].text_content():
+            speed = td[6].getchildren()[0].attrib["title"].strip().replace(u"秒","")
+            sur_time = td[8].text_content()
+            if float(speed) < 1 and u"分钟" not in sur_time:
+                result.append([td[1].text_content(),td[2].text_content()])
+    return result if result else None
 
-def verify_ip_status(ip):
-        if isinstance(ip,list) and ip:
-            proxy_convert = "%s:%s"%(ip[0],str(ip[1]))
-            proxy = {"http":proxy_convert,"https":proxy_convert}
-            try:
-                telnetlib.Telnet(ip[0], port=str(ip[1]), timeout=10)
-                r = requests.get("http://weixin.sogou.com/", timeout=10, proxies=proxy)
-            except:
-                return None
-            try:
-                r = requests.get("https://www.baidu.com", timeout=10, proxies=proxy)
-                return "HTTPS:%s" % proxy_convert
-            except:
-                return "HTTP:%s" % proxy_convert
-        else:
-            print "[ERROR]Params %s is not a list or is null..."%(ip)
+
+def verify_ip_status(ip, dic_url):
+    dic_url = {"item":"taobao","url":"https://www.taobao.com/"}
+    timeout = 3
+    if isinstance(ip,list) and ip:
+        proxy_convert = "%s:%s"%(ip[0],str(ip[1]))
+        proxy = {"http":proxy_convert,"https":proxy_convert}
+        # try:
+        #     #telnetlib.Telnet(ip[0], port=str(ip[1]), timeout=timeout)
+        #     r = requests.get("http://weixin.sogou.com/", timeout=timeout, proxies=proxy)
+        # except:
+        #     return None
+        try:
+            r = requests.get(dic_url["url"], timeout=timeout, proxies=proxy)
+            return "%s:%s" % (dic_url["item"],proxy_convert)
+        except:
+            #return "HTTP:%s" % proxy_convert
             return None
+    else:
+        print "[ERROR]Params %s is not a list or is null..."%(ip)
+        return None
 
 
 def store_proxy(proxy,redis_conn):
@@ -125,21 +140,29 @@ def store_proxy(proxy,redis_conn):
         print "[ERROR]Params %s is not a list or is null..." %(proxy)
 
 
-def clear_ip(proxy, redis_conn):
+def clear_ip(proxy, redis_conn,dic_url):
     item = proxy.split(":")
     del item[0]
     if item:
-        if verify_ip_status(item):
+        if verify_ip_status(item,dic_url):
             print "[INFO]Proxy %s will be cleared..." % (proxy)
             redis_conn.delete(proxy)
 
+
+def get_random_proxy(redis_conn):
+    try:
+        result = redis_conn.randomkey()
+    except:
+        return None
+    return result
 
 
 
 if __name__ == "__main__":
     #tmp = Base()
     ip_list = Base.get_dynamic_source("http://www.xicidaili.com/nt/", parse_func_1)
-    ip1,ip2 = verify_ip_status(ip_list)
+    dic_url = {"item": "taobao", "url": "https://www.taobao.com/"}
+    ip1,ip2 = verify_ip_status(ip_list,dic_url)
     print ip1,ip2
     # tmp.get_source("http://www.xicidaili.com/wt/2")
     # page_source = ""
--- getproxy.py
+++ getproxy.py
@@ -5,10 +5,11 @@ Created on Jan 28, 2018
 @author: guxiwen
 """
 
-from base import Base,verify_ip_status,store_proxy,parse_func_1,clear_ip
+from base import Base,verify_ip_status,store_proxy,parse_func_1,clear_ip,get_random_proxy
 from Queue import Queue
 import threading,time,redis
 
+lock = threading.Lock()
 
 class MyQueue(Queue):
     def __init__(self):
@@ -18,11 +19,47 @@ class MyQueue(Queue):
         if item not in self.queue:
             self.queue.append(item)
 
-class ProxyConsumer(threading.Thread):
-    def __init__(self,queue,queue_to_save):
+
+class ProxyProduct(threading.Thread):
+    num = 1
+    def __init__(self, queue_to_filter, redis_conn,max_num,func=None):
         threading.Thread.__init__(self)
-        self.data = queue
+        self.data_to_filter = queue_to_filter
+        self.time_delay = 30
+        self.func = func
+        self.redis_conn = redis_conn
+        self.max_num = max_num
+
+    def run(self):
+        if self.func:
+            while True:
+                print "Queue_to_filter size is %s" %(self.data_to_filter.qsize())
+                try:
+                    lock.acquire()
+                    ProxyProduct.num = ProxyProduct.num + 1
+                    if ProxyProduct.num > self.max_num:
+                        print "Provide proxy job is over...Finishing..."
+                        return
+                    page = ProxyProduct.num
+                except:
+                    print "[ERROR]Get lock failed..."
+                finally:
+                    lock.release()
+                proxies = self.func(self.redis_conn,page)
+                if proxies:
+                    for proxy in proxies:
+                        self.data_to_filter.put(proxy)
+                time.sleep(self.time_delay)
+        else:
+            print "[ERROR]No func!"
+
+
+class ProxyConsumer(threading.Thread):
+    def __init__(self,queue_to_filter, queue_to_save):
+        threading.Thread.__init__(self)
+        self.data = queue_to_filter
         self.data_to_save = queue_to_save
+        self.dic_url = {"item":"taobao","url":"https://www.taobao.com/"}
 
     def run(self):
         while True:
@@ -30,11 +67,12 @@ class ProxyConsumer(threading.Thread):
                 time.sleep(3)
             else:
                 proxy = self.data.get()
-                proxy_convert = verify_ip_status(proxy)
+                proxy_convert = verify_ip_status(proxy,self.dic_url)
                 if proxy_convert:
                     self.data_to_save.put(proxy_convert)
                 else:
                     continue
+
 
 class ProxySave(threading.Thread):
     def __init__(self, queue_to_save):
@@ -46,7 +84,7 @@ class ProxySave(threading.Thread):
         while True:
             print "Save queue size %d" % self.data.qsize()
             if self.data.qsize() == 0:
-                time.sleep(10)
+                time.sleep(60)
             else:
                 proxy = self.data.get()
                 store_proxy(proxy,self.redis_conn)
@@ -58,17 +96,18 @@ class ProxyClearProduct(threading.Thread):
         self.data = queue_to_clear
         self.redis_conn = redis.Redis(host="127.0.0.1",db=0)
         self.time_delay = 1800
+        self.dic_url = {"item":"taobao","url":"https://www.taobao.com/"}
 
     def run(self):
         while True:
-            keys = self.redis_conn.keys("HTTP*")
+            keys = self.redis_conn.keys(self.dic_url["item"]+"*")
             for key in keys:
                 self.data.put(key)
             time.sleep(self.time_delay)
 
 
 class ProxyClear(threading.Thread):
-    def __init__(self,queue_to_clear):
+    def __init__(self, queue_to_clear):
         threading.Thread.__init__(self)
         self.redis_conn = redis.Redis(host="127.0.0.1", db=0)
         self.data = queue_to_clear
@@ -80,14 +119,29 @@ class ProxyClear(threading.Thread):
             clear_ip(proxy,self.redis_conn)
         time.sleep(self.time_delay)
 
+
+def get_xici_ip(redis_conn,page):
+    url = "http://www.xicidaili.com/nt/" + str(page)
+    ip_list = Base.get_dynamic_source(url, parse_func_1)
+    if ip_list == "1010":
+        proxy = get_random_proxy(redis_conn)
+        print "[INFO]Get proxy %s ..." % (proxy)
+        if not proxy:
+            selenium_proxy = proxy.split(":")
+            del selenium_proxy[0]
+            ip_list = Base.get_dynamic_source(url, parse_func_1, selenium_proxy)
+            return ip_list
+    return ip_list if ip_list else None
+
 if __name__ == "__main__":
     queue_to_filter = MyQueue()
     queue_to_save = MyQueue()
     queue_to_clear = MyQueue()
-    ip_list = Base.get_dynamic_source("http://www.xicidaili.com/nt/", parse_func_1)
-    for ip in ip_list:
-        queue_to_filter.put(ip)
-    for i in range(6):
+    redis_conn = redis.Redis(host="127.0.0.1", db=0)
+    for i in range(2):
+        proxy_p = ProxyProduct(queue_to_filter,redis_conn,630,get_xici_ip)
+        proxy_p.start()
+    for i in range(8):
         proxy_c = ProxyConsumer(queue_to_filter,queue_to_save)
         proxy_c.start()
     proxy_save = ProxySave(queue_to_save)
